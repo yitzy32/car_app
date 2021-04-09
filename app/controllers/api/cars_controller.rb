09bd6ee28ebb2c9079cf47bbdb152663f5518cfe
Yitzy Bratt <yitzybratt32@gmail.com>
2021-04-09 11:54:08 -0400

    update action

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -17,4 +17,12 @@ class Api::CarsController < ApplicationController
     @car.save
     render "show.json.jb"
   end
+
+  def update
+    @car = Car.find_by(id: params[:id])
+    @car.color = params[:color] || @car.color
+    @car.year = params[:year] || @car.year
+    @car.save
+    render "show.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     get "/cars" => "cars#index"
     get "/cars/:id" => "cars#show"
     post "/cars" => "cars#create"
+    patch "/cars/:id" => "cars#update"
   end
 end
